cefcbfa759693f51268be21c580d4e2e3570ca97
daiyunjiao <daiyunjiao@xinchao.com>
2020-05-06 14:23:24 +0800

    add nacos config

--- spring-nacos-project/README.md
+++ spring-nacos-project/README.md
@@ -5,5 +5,9 @@ curl http://127.0.0.1:9662/echo/provider/a
 
 curl http://127.0.0.1:9661/echo/consumer
 
+curl http://127.0.0.1:9662/config/get
+
+curl http://127.0.0.1:9661/config/get
+
 ls
 ```
---
+++ spring-nacos-project/spring-nacos-project-consumer/src/main/java/com/bzmxl/nacos/consumer/controller/ConfigController.java
@@ -0,0 +1,20 @@
+package com.bzmxl.nacos.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${consumer.config.name}")
+    private String useLocalCache;
+
+    @RequestMapping("/get")
+    public String get() {
+        return useLocalCache;
+    }
+}
