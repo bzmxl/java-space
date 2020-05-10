62d3d2f226e7055c2f6852dd5067f3019c55758f
bzmxl <bzmxl@outlook.com>
2020-05-10 22:42:13 +0800

    refine hystrix consumer

--- spring-hystrix-consumer/spring-hystrix-consumer-provider/src/main/java/co/consumer/provider/CoApplication.java
+++ spring-hystrix-consumer/spring-hystrix-consumer-provider/src/main/java/co/consumer/provider/CoApplication.java
@@ -2,12 +2,14 @@ package co.consumer.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class CoApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoApplication.class, args);
